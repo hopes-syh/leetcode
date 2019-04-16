8be155b932c09e26e2e28f0c6f75e2f0f53c1d9a
leo <1368975930@qq.com>
2019-04-17 03:05:19 +0800

    test commit

--- src/com/syh/heap/KthLargestElementInAnArray.java
+++ src/com/syh/heap/KthLargestElementInAnArray.java
@@ -16,7 +16,7 @@ public class KthLargestElementInAnArray {
         System.out.println(k.findKthLargest(new int[]{3,2,3,1,2,4,5,5,6}, 4));
     }
 
-    public int findKthLargest(int[] nums, int k) {    
+    public int findKthLargest(int[] nums, int k) {
         if(nums == null || nums.length <= 0){
             return 0;
         }
