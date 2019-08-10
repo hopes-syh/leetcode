befcccf3dff8a06c537bfe2f367b5fef19820021
leo <1368975930@qq.com>
2019-08-10 18:59:54 +0800

    https://leetcode.com/problems/unique-binary-search-trees-ii/

--- src/com/syh/dynamic/UniqueBinarySearchTrees2.java
+++ src/com/syh/dynamic/UniqueBinarySearchTrees2.java
@@ -1,5 +1,7 @@
 package com.syh.dynamic;
 
+import sun.reflect.generics.tree.Tree;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -12,7 +14,7 @@ import java.util.List;
  * Time: 下午3:08
  * To change this template use File | Settings | File Templates.
  *
- * https://leetcode-cn.com/problems/unique-binary-search-trees-ii/description/
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/
  */
 public class UniqueBinarySearchTrees2 {
 
@@ -20,6 +22,7 @@ public class UniqueBinarySearchTrees2 {
     public static void main(String[] args) {
         UniqueBinarySearchTrees2 st = new UniqueBinarySearchTrees2();
 
+        System.out.println(st.generateTrees(1));
         System.out.println(st.generateTrees(2));
         System.out.println(st.generateTrees(3));
         System.out.println(st.generateTrees(4));
@@ -27,29 +30,57 @@ public class UniqueBinarySearchTrees2 {
     }
 
     public List<TreeNode> generateTrees(int n) {
-        List<TreeNode> treeNodeList = new ArrayList<TreeNode>();
-        if(n<=0){
-            return treeNodeList;
+        List<TreeNode> treeList = new ArrayList<>();
+        if(n <= 0){
+            return treeList;
+        }
+        treeList.add(null);
+
+        for(int i=1; i<=n; i++){
+            List<TreeNode> cur = new ArrayList<TreeNode>();
+
+            for(TreeNode root : treeList) {
+                // 添加到根节点
+                TreeNode insert = new TreeNode(i);
+                insert.left = root;
+                cur.add(insert);
+
+                for(int j=0; j<=n; j++){
+                    TreeNode rootCopy = copyTree(root); //复制当前的树
+                    TreeNode right = rootCopy; //找到要插入右孩子的位置
+
+                    for(int k=0; k<j; k++){
+                        if(right == null){
+                            break;
+                        }
+                        right = right.right;
+                    }
+
+                    if(right == null){
+                        break;
+                    }
+                    TreeNode rightNode = right.right;
+                    insert = new TreeNode(i);
+                    right.right = insert;
+                    insert.left = rightNode;
+                    cur.add(rootCopy);
+                }
+            }
+
+            treeList = cur;
         }
 
-        int[] indexs = new int[n];
-        for(int i=1; i<=n; i++){
-            indexs[i] = i;
-        }
-        for(int i=1; i<=n; i++){
-            int[] left = Arrays.copyOfRange(indexs, 0, i);
-            int[] right = Arrays.copyOfRange(indexs, i, n);
-            generateTrees(treeNodeList, i, left, right);
-        }
-
-        return treeNodeList;
+        return treeList;
     }
 
-    private void generateTrees(List<TreeNode> treeNodeList, int n, int[] left, int[] right) {
-
-        TreeNode node = new TreeNode(n);
-
-
+    public TreeNode copyTree(TreeNode node){
+        if(null == node){
+            return null;
+        }
+        TreeNode treeNode = new TreeNode(node.val);
+        treeNode.left = copyTree(node.left);
+        treeNode.right = copyTree(node.right);
+        return treeNode;
     }
 
     public class TreeNode {
@@ -60,10 +91,20 @@ public class UniqueBinarySearchTrees2 {
 
         @Override
         public String toString() {
-            if(val <= 0){
-                return "null";
+            StringBuilder sb = new StringBuilder();
+            value(sb, this);
+            return sb.toString();
+        }
+
+        private void value(StringBuilder sb, TreeNode treeNode){
+            sb.append(null == treeNode ? "null" : treeNode.val);
+            sb.append("-");
+            if(null == treeNode){
+                return;
             }
-            return String.valueOf(val);
+
+            value(sb, treeNode.left);
+            value(sb, treeNode.right);
         }
     }
 
